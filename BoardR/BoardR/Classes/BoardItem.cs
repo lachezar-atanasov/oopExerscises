9187951baefcb127d9d6fde9c7d42d1951da671e
lachezar-atanasov <lachezar.atanasov99@gmail.com>
2024-04-04 13:12:32 +0300

    BoardR2 version added

--- BoardR/BoardR/Classes/BoardItem.cs
+++ BoardR/BoardR/Classes/BoardItem.cs
@@ -1,34 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BoardR.Classes
 {
     public class BoardItem
     {
-        public string title;
-        public DateTime dueDate;
-        public Status status;
-        
+        private string title;
+        private DateTime dueDate;
+        private Status status;
+        private readonly List<EventLog> logs = new List<EventLog>();
+        private const string dateTimeFormat = "dd-MM-yyyy";
 
         public BoardItem(string title, DateTime dueDate)
         {
-            bool isValidTitle = title.Length >= 5 && title.Length <= 30;
-            bool isValidDate = dueDate > DateTime.Now;
-
-            if (isValidTitle)
-            {
-                this.title = title;
-            }
-
-            if (isValidDate)
-            {
-                this.dueDate = dueDate;
-            }
-
+            Title = title;
+            DueDate = dueDate;
             status = Status.Open;
+            logs.Add(new EventLog($"Item created: '{this.ViewInfo()}'"));
+        }
+
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                bool isValidTitle = value.Length >= 5 && value.Length <= 30;
+
+                if (!isValidTitle)
+                {
+                    throw new ArgumentException("Title must be between 5 and 30 chars.");
+                }
+
+                // set only if validated, otherwise throw ex with some meaningful message
+                if (!String.IsNullOrEmpty(title))
+                {
+                    logs.Add(new EventLog($"{nameof(Title)} changed from '{title}' to '{value}'"));
+                }
+                
+                this.title = value;
+                
+            }
+        }
+        public DateTime DueDate
+        {
+            get
+            {
+                return this.dueDate;
+            }
+            set
+            {
+                bool isValidDate = value > DateTime.Now;
+
+                if (!isValidDate)
+                {
+                    throw new ArgumentException("Date must be in the future.");
+                }
+
+                // set only if validated, otherwise throw ex with some meaningful message
+                if (!(dueDate==default(DateTime)))
+                {
+                    logs.Add(new EventLog($"{nameof(DueDate)} changed from " +
+                        $"'{dueDate.ToString(dateTimeFormat)}' to '{value.ToString(dateTimeFormat)}'"));
+                }
+                this.dueDate = value;
+            }
+        }
+        public Status Status
+        {
+            get
+            {
+                // return the value of the status field
+                return this.status;
+            }
         }
         public string ViewInfo()
         {
@@ -39,19 +90,29 @@ namespace BoardR.Classes
         {
             if (status == 0)
             {
+                logs.Add(new EventLog($"Can't revert, already at {status}"));
                 return;
             }
-            status--;
+            logs.Add(new EventLog($"Status changed from {status} to {--status}"));
         }
-
         public void AdvanceStatus()
         {
             bool isLastStatusElement = (int)status == Enum.GetNames(typeof(Status)).Length - 1;
             if (isLastStatusElement)
             {
+                logs.Add(new EventLog($"Can't advance, already at {status}"));
                 return;
             }
-            status++;
+            logs.Add(new EventLog($"Status changed from {status} to {++status}"));
+        }
+        public string ViewHistory()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (EventLog log in logs) 
+            {
+                sb.AppendLine(log.ViewInfo());
+            }
+            return sb.ToString();
         }
     }
 }
